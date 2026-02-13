8e8f6d88fbf43950db38528ca5ffb363d217895e
DuyLong <duylongmind432001@gmail.com>
2026-02-13 20:54:52 +0700

    Commit before push

--- android/app/src/main/kotlin/com/example/notion_2/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.ice_shield
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/initial_layer/ThemeLayer/CurrentThemeData.dart
+++
@@ -1,6 +0,0 @@
-class CurrentThemeData {
-  int? themeID;
-  String? themeName;
-  String themePath;
-  CurrentThemeData({this.themeID, this.themeName, required this.themePath});
-}
